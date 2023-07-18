d0f4268e6a1451e48ef292514e5ab9dff9da3731
Bolshiyanov <bolshiyanov@gmail.com>
2023-07-18 15:22:38 +0100

    Added get req to Json placeholder server with out thunk

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,63 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import { useAppDispatch, useAppSelector } from "./utils/hooks/redux";
+import { counterSlice } from "./store/reducers/CounterSlice";
+import { fetchUsers } from "./store/reducers/ActionCreator";
 
 import "./App.css";
-import { useAppDispatch, useAppSelector } from "./hooks/redux";
-import { counterSlice } from "./store/reducers/CounterSlice";
+import { userSlice } from "./store/reducers/UserSlice";
 
-const  App = () => {
+const App = () => {
+  const [getUsers, setGetUsers] = useState(false);
   const { increment, decrement } = counterSlice.actions;
   const dispatch = useAppDispatch();
   const { count } = useAppSelector((state) => state.counterReducer);
+  const { users, isLoading, error } = useAppSelector(
+    (state) => state.userReducer
+  );
+
+  const getUser = () => {
+    setGetUsers(true);
+  };
+
+  const clearData = () => {
+    dispatch(userSlice.actions.userFetchingSucces([]));
+    setGetUsers(false);
+  };
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, [dispatch, getUsers]);
 
   return (
     <div className="App">
       <h1>{count}</h1>
+      {/* here logic reducer */}
       <button onClick={() => dispatch(increment(1))}> INCREMENT</button>
       <button onClick={() => dispatch(decrement(1))}> DECREMENT</button>
-    <div className="link"><a href="https://github.com/bolshiyanov/redux_toolkit_rtk_query_pro_template">Code Git here</a></div>
+      <br />
+      <br />
+      {/* here reuducer with out Thunk */}
+      {getUsers ? (
+        <>
+          <button onClick={clearData}>Clear users</button>
+          <div>
+            {isLoading && <h2>Loading...</h2>}
+            {error && <h2>{error}</h2>}
+            {JSON.stringify(users, null, 2)}
+          </div>
+        </>
+      ) : (
+        <button onClick={getUser}>Get user with axios, without thunk</button>
+      )}
+
+      <div className="link">
+        <a href="https://github.com/bolshiyanov/redux_toolkit_rtk_query_pro_template">
+          Code Git here
+        </a>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
--- src/store/reducers/UserSlice.ts
+++ src/store/reducers/UserSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice} from "@reduxjs/toolkit";
+import { createSlice, PayloadAction} from "@reduxjs/toolkit";
 import { IUser } from "../../models/IUsers";
 
 interface UserState {
@@ -17,6 +17,18 @@ export const userSlice = createSlice({
   name: "user",
   initialState,
   reducers: {
+    userFetching(state) {
+        state.isLoading = true;
+    },
+    userFetchingSucces(state, action: PayloadAction<IUser[]>) {
+        state.isLoading = false;
+        state.error = '';
+        state.users= action.payload;
+    },
+    userFetchingError(state, action: PayloadAction<string>) {
+        state.isLoading = false;
+        state.error = action.payload;
+    },
   },
 });
 
